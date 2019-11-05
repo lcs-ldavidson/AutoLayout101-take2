f548eeeca2c2065c5b24a128ade561cc96e560a6
Liam Davidson <ldavidson@lcs.on.ca>
2019-11-05 16:38:11 -0500

    Initial Commit

---
+++ AutoLayout101-take2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayout101-take2
+//
+//  Created by Davidson, Liam on 2019-11-05.
+//  Copyright © 2019 Davidson, Liam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
